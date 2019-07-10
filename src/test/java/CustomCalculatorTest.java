337a477261b2bab1a8d5787f01f401edee280dc8
YongJae <sdc689@gmail.com>
2019-07-10 17:39:10 +0900

    Add assignment code

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -1,5 +1,6 @@
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -14,7 +15,8 @@ public class CustomCalculatorTest {
     public void add() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        //assertTrue(result == 25);
+        assertThat(result, is(25));
         System.out.println("result :: " + result);
     }
 
@@ -23,7 +25,7 @@ public class CustomCalculatorTest {
     public void subtract() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(13));
         System.out.println("result :: " + result);
     }
 
@@ -32,7 +34,7 @@ public class CustomCalculatorTest {
     public void multiply() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(45));
         System.out.println("result :: " + result);
     }
 
@@ -41,7 +43,7 @@ public class CustomCalculatorTest {
     public void divide() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(5));
         System.out.println("result :: " + result);
     }
 }
